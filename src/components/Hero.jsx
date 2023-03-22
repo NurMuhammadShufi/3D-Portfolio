beeb2f33654d1dcda90928d0ef30630795ed9f6d
Nur Muhammad Shufi <shufimuhammad013@gmail.com>
2023-03-22 15:19:38 +0700

    Finish Hero.jsx Section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -29,6 +29,26 @@ const Hero = () => {
       {/* 3D Computer Canvas Start*/}
       <ComputersCanvas />
       {/* 3D Computer Canvas End*/}
+
+      {/* Scroll Button Start */}
+      <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
+        <a href="#about">
+          <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
+            <motion.div
+              animate={{
+                y: [0, 24, 0],
+              }}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: "loop",
+              }}
+              className="w-3 h-3 rounded-full bg-secondary mb-1"
+            />
+          </div>
+        </a>
+      </div>
+      {/* Scroll Button End */}
     </section>
   );
 };
